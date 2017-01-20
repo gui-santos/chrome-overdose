f5f933aec4ae549c6cee3ae898ecf249a22d53b2
Guilherme Santos <guilherme.goncalvessantos@gmail.com>
2017-01-19 23:36:40 -0200

    writing some boilerplate

---
+++ src/js/main.js
@@ -0,0 +1,15 @@
+var game = new Phaser.Game(800, 600, Phaser.AUTO, 'game', { preload: preload, create: create, update: update });
+
+function preload() {
+
+}
+
+// variables
+
+function create() {
+
+}
+
+function update() {
+
+}
